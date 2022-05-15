d4bd341031f54c313822f0cf1de23638ef21a54b
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-05-15 17:55:48 -0300

    adiciona o arquivo teste.js

---
+++ FundamentosDev/4.Intro-JS-LogicaProgramacao/4-3.JS-Lógica-de-Programação-e-Algoritimos/teste.js
@@ -0,0 +1,11 @@
+let fruits = [3, 4, 10, 1, 12];
+let result = 0;
+
+for (i = 0; i < fruits.length; i += 1){  
+  result += fruits[i];
+}
+    if(result > 15){
+      console.log(result);
+    }else{
+      console.log('valor menor que 16');  
+    }
