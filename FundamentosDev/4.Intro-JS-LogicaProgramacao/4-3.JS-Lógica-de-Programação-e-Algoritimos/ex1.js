2b250efd985f7aab35dae6a957e5b0e327c7058c
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-05-16 18:55:32 -0300

    adiciona ex1

--- FundamentosDev/4.Intro-JS-LogicaProgramacao/4-3.JS-Lógica-de-Programação-e-Algoritimos/ex1.js
+++ FundamentosDev/4.Intro-JS-LogicaProgramacao/4-3.JS-Lógica-de-Programação-e-Algoritimos/ex1.js
@@ -1,17 +1,25 @@
 // 1- O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero.
 
-let num = 'bc';
-let fatorial = num;
-let multiplicador = fatorial - 1;
+// let num = 6;
+// let fatorial = num;
 
-for (let i = multiplicador; i > 0; i -= 1) {
-    fatorial *= i;
+
+
+function fatora(fatorial){
+    if (fatorial === 0 || fatorial === 1){
+        console.log(1);
+    }else if (isNaN(fatorial)){
+        console.log('Não existe fatorial de textos');
+    }else if (!Number.isInteger(fatorial) || fatorial < 0) {
+        console.log('Não existe fatorial de um número não natural');
+    }else{
+        let multiplicador = fatorial - 1;
+        for (let i = multiplicador; i > 0; i -= 1) {
+            fatorial *= i;  
+        }
+    }
+    return fatorial
 }
-if (fatorial === 0 || fatorial === 1){
-    console.log(1);
-}else if (fatorial = NaN){
-    console.log('não existe fatorial de textos');
-}
+console.log(fatora(4));
 
 
-console.log(fatorial);
