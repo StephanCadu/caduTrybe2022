e4cbdf2ea8298fba2abf50e0b125471c252c84b5
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-05-20 18:52:28 -0300

    cumpre o exercício 2

--- FundamentosDev/5.JS-DOM-Eventos-WebStorage/5-2.JS-Trabalhando-com-elementos/exercicios.js
+++ FundamentosDev/5.JS-DOM-Eventos-WebStorage/5-2.JS-Trabalhando-com-elementos/exercicios.js
@@ -6,3 +6,10 @@ let aga1 = document.createElement('h1');
 aga1.innerText = 'Exercício 5.2 - JavaScript DOM';
 
 corpo.appendChild(aga1)
+
+// EXERCÍCIO 2
+
+let main = document.createElement('main');
+main.className = 'main-content';
+
+corpo.appendChild(main);
