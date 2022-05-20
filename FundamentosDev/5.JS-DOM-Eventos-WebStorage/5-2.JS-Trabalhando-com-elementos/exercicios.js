4cc07c22f42f95d1cc2b4ea7768447718a2e0727
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-05-20 18:46:04 -0300

    adiciona arquivos de exercicios do bloco 5 dia 2 junto com o exercício 1

---
+++ FundamentosDev/5.JS-DOM-Eventos-WebStorage/5-2.JS-Trabalhando-com-elementos/exercicios.js
@@ -0,0 +1,8 @@
+ //   EXERCÍCIO 1
+
+let corpo = document.querySelector('body');
+
+let aga1 = document.createElement('h1');
+aga1.innerText = 'Exercício 5.2 - JavaScript DOM';
+
+corpo.appendChild(aga1)
