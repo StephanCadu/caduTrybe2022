6ca0813da9b4006e3d3a3cb9e803ea4150fce071
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-06-23 19:05:33 -0300

    exercício de prática 3

---
+++ FundamentosDev/8.JSES6-HigherOrderFunctions/8-4.JSES6-Higher-Order-Functions-reduce/practice3.js
@@ -0,0 +1,43 @@
+const personnel = [
+    {
+      id: 5,
+      name: "Luke Skywalker",
+      pilotingScore: 98,
+      shootingScore: 56,
+      isForceUser: true,
+    },
+    {
+      id: 82,
+      name: "Sabine Wren",
+      pilotingScore: 73,
+      shootingScore: 99,
+      isForceUser: false,
+    },
+    {
+      id: 22,
+      name: "Zeb Orellios",
+      pilotingScore: 20,
+      shootingScore: 59,
+      isForceUser: false,
+    },
+    {
+      id: 15,
+      name: "Ezra Bridger",
+      pilotingScore: 43,
+      shootingScore: 67,
+      isForceUser: true,
+    },
+    {
+      id: 11,
+      name: "Caleb Dume",
+      pilotingScore: 71,
+      shootingScore: 85,
+      isForceUser: true,
+    },
+  ];
+
+// get the total score of force users only
+
+const forceScore = personnel.reduce((acc, obj) => obj.isForceUser ? acc + obj.shootingScore + obj.pilotingScore : acc, 0);
+
+console.log(forceScore);
