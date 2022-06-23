7288883b8906eeebfa3a753c5f76d675413213dd
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-06-23 15:05:45 -0300

    adiciona duas soluções alternativas ao ex 1 (spread e flat)

--- FundamentosDev/8.JSES6-HigherOrderFunctions/8-4.JSES6-Higher-Order-Functions-reduce/exercise1.js
+++ FundamentosDev/8.JSES6-HigherOrderFunctions/8-4.JSES6-Higher-Order-Functions-reduce/exercise1.js
@@ -4,8 +4,17 @@ const arrays = [
   [4, 5, 6],
 ];
 
+// solução com concat
 const flatten = () => arrays.reduce(((a, b) => a.concat(b)), []);
 
 console.log(flatten());
 
-// nota pra lembrar de voltar nesse exercício e realizar apenas com reduce
+// solução com spread
+const flatten2 = () => arrays.reduce(((a, b) => [...a, ...b]), []);
+
+console.log(flatten2());
+
+// solução com flat
+const flatten3 = () => arrays.flat();
+
+console.log(flatten3());
