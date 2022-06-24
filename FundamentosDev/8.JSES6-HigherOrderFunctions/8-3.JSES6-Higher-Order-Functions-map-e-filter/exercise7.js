b9451cc1aab1c394b807643302402cdccc06a559
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-06-24 12:11:51 -0300

    modifica ex 7

--- FundamentosDev/8.JSES6-HigherOrderFunctions/8-3.JSES6-Higher-Order-Functions-map-e-filter/exercise7.js
+++ FundamentosDev/8.JSES6-HigherOrderFunctions/8-3.JSES6-Higher-Order-Functions-map-e-filter/exercise7.js
@@ -8,3 +8,16 @@ console.log(authorWith3DotsOnName(books));
 // console.log('George R. R. Martin'.split('').filter((char) => char === '.'));
 
 // ref que ajudou na resolução: https://pt.stackoverflow.com/questions/486918/como-contar-ocorr%C3%AAncias-de-uma-letra-em-uma-frase#:~:text=%C3%89%20poss%C3%ADvel%20utilizar%20o%20m%C3%A9todo,String%20ou%20uma%20express%C3%A3o%20regular.
+
+
+const nome = 'Qualquer. Coisa.'
+
+const arrNome = nome.split('');
+
+console.log(arrNome);
+
+const pontos = arrNome.filter((caracter) => caracter === '.');
+
+console.log(pontos);
+
+console.log(pontos.length);
