5f1a0ac71c609ad4374b97ba11af2377a75c6f97
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-06-27 10:29:44 -0300

    modifica ex 9 e prática de rest

--- FundamentosDev/8.JSES6-HigherOrderFunctions/8-5.JSES6-Spread-operator-parâmetro-test-destructuring-e-mais/exercise9.js
+++ FundamentosDev/8.JSES6-HigherOrderFunctions/8-5.JSES6-Spread-operator-parâmetro-test-destructuring-e-mais/exercise9.js
@@ -11,3 +11,30 @@ const { spring, summer, autumn, winter } = yearSeasons;
 const months = [...spring, ...summer, ...autumn, ...winter];
 
 console.log(months);
+
+const cart = [
+    {
+      name: "laptop",
+      price: 1000,
+      quantity: 2,
+      id: 3,
+    },
+    {
+      name: "desktop",
+      price: 1500,
+      quantity: 1,
+      id: 2,
+    },
+    {
+      name: "phone",
+      price: 500,
+      quantity: 3,
+      id: 1,
+    },
+  ];
+  const valorInicial = 0;
+  const totalCart = cart.reduce((accumulator, product) => accumulator + product.price * product.quantity, valorInicial);
+  
+
+console.log(totalCart);
+  
--- FundamentosDev/8.JSES6-HigherOrderFunctions/8-5.JSES6-Spread-operator-parâmetro-test-destructuring-e-mais/rest.js
+++ FundamentosDev/8.JSES6-HigherOrderFunctions/8-5.JSES6-Spread-operator-parâmetro-test-destructuring-e-mais/rest.js
@@ -3,14 +3,8 @@
 // permite que sejam passados vários parâmetros para uma função
 // empacota todos os parâmetros em um array
 
-const quantosParam = (...par) => console.log(`Parâmetros: ${par}, 
-Quantidade: ${par.length}`);
+const firstArray = [3, 5, 7];
 
-quantosParam(1, 2, 'a', false, { a: 3 }, [2, 3, 4]);
+const secondArray = [2, ...firstArray, 4];
 
-// pode ser bem útil em uma função que soma todos os números passados
-
-const sum = (...nums) => nums.reduce((a, b) => a + b);
-
-console.log(sum(1, 3, 4));
-console.log(sum(1, 2, 3, 4, 5, 6, 7));
+console.log(secondArray);
