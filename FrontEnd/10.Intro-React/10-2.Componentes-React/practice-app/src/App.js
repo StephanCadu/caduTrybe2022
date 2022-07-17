6cd19a1c6e3f347a6f1191f6b8c0ee5f87bbc054
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-07-16 23:21:39 -0300

    adiciona exemplo de lista de componentes

--- FrontEnd/10.Intro-React/10-2.Componentes-React/practice-app/src/App.js
+++ FrontEnd/10.Intro-React/10-2.Componentes-React/practice-app/src/App.js
@@ -123,4 +123,19 @@ import './App.css';
 //   }
 // }
 
-// export default App;
+// exemplo lista de componentes
+class App extends React.Component {
+  render() {
+    const shoppingList = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
+    const items = shoppingList.map((item, ind) => (<li key={ind}>{ item }</li>));
+  
+    return (
+      <div>
+        <h2>Lista de compras</h2>
+        <ul>{items}</ul>
+      </div>
+    );
+  }
+}
+
+export default App;
