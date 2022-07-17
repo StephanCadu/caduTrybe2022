65708668a6aa792fdc5b280335457ca39f813680
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-07-16 23:31:26 -0300

    adiciona exercício de fixação lista de componentes e chaves

--- FrontEnd/10.Intro-React/10-2.Componentes-React/practice-app/src/App.js
+++ FrontEnd/10.Intro-React/10-2.Componentes-React/practice-app/src/App.js
@@ -44,8 +44,6 @@ import './App.css';
 //   </div>
 //  );
 
-// arquivo App.js, criado pelo create-react-app, modificado
-
 // exemplo componentes sendo repassados
 // import UserProfile from './UserProfile';
 
@@ -73,9 +71,6 @@ import './App.css';
 //     );
 //   }
 // }
-
-// arquivo App.js, criado pelo create-react-app, modificado
-
 
 // exercício final de composicão de componentes
 // import Order from './Order';
@@ -124,15 +119,44 @@ import './App.css';
 // }
 
 // exemplo lista de componentes
+// class App extends React.Component {
+//   render() {
+//     const shoppingList = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
+//     const items = shoppingList.map((item, ind) => (<li key={ind}>{ item }</li>));
+  
+//     return (
+//       <div>
+//         <h2>Lista de compras</h2>
+//         <ul>{items}</ul>
+//       </div>
+//     );
+//   }
+// }
+
+// exercício de fixação lista de componentes e chaves
+import UserProfile from './UserProfile';
+
 class App extends React.Component {
   render() {
-    const shoppingList = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
-    const items = shoppingList.map((item, ind) => (<li key={ind}>{ item }</li>));
-  
+    const users = [
+      {
+        id: 102,
+        name: "João",
+        email: "joao@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+      },
+      {
+        id: 77,
+        name: "Amélia",
+        email: "amelia@gmail.com",
+        avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+      },
+    ]
     return (
-      <div>
-        <h2>Lista de compras</h2>
-        <ul>{items}</ul>
+      <div className="App">
+        {/* criando os usuários dinamicamente e
+        adicionando uma chave pra cada */}
+        {users.map((user) => <UserProfile user={user} key={user.id} />)}
       </div>
     );
   }
