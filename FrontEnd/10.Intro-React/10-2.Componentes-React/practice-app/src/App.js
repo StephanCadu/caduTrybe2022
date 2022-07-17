c0056eaf5b9f09bdf806c2175f739fd3d42d6d71
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-07-16 23:13:08 -0300

    adiciona exercício final de composição de componentes

--- FrontEnd/10.Intro-React/10-2.Componentes-React/practice-app/src/App.js
+++ FrontEnd/10.Intro-React/10-2.Componentes-React/practice-app/src/App.js
@@ -46,32 +46,81 @@ import './App.css';
 
 // arquivo App.js, criado pelo create-react-app, modificado
 
-import './App.css';
-import UserProfile from './UserProfile';
+// exemplo componentes sendo repassados
+// import UserProfile from './UserProfile';
 
-class App extends React.Component {
-  render() {
-    const joao = {
-      id: 102,
-      name: "João",
-      email: "joao@gmail.com",
-      avatar: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
-    };
+// class App extends React.Component {
+//   render() {
+//     const joao = {
+//       id: 102,
+//       name: "João",
+//       email: "joao@gmail.com",
+//       avatar: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
+//     };
 
-    const amelia = {
-      id: 77,
-      name: "Amélia",
-      email: "amelia@gmail.com",
-      avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
-    };
+//     const amelia = {
+//       id: 77,
+//       name: "Amélia",
+//       email: "amelia@gmail.com",
+//       avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+//     };
 
-    return (
-      <div className="App">
-        <UserProfile user={joao} />
-        <UserProfile user={amelia} />
-      </div>
-    );
-  }
-}
+//     return (
+//       <div className="App">
+//         <UserProfile user={joao} />
+//         <UserProfile user={amelia} />
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+// arquivo App.js, criado pelo create-react-app, modificado
+
+
+// exercício final de composicão de componentes
+// import Order from './Order';
+
+// class App extends React.Component {
+//   render() {
+//     const headphone = {
+//       id: 102,
+//       user: "cena@gmail.com",
+//       product: "Razer Headphone",
+//       price: {
+//         value: 99.99,
+//         currency: "dollars"
+//       }
+//     };
+
+//     const energyDrink = {
+//       id: 77,
+//       user: "cena@gmail.com",
+//       product: "Monster 500mL",
+//       price: {
+//         value: 9.99,
+//         currency: "dollars"
+//       }
+//     };
+
+//     const shape = {
+//       id: 69,
+//       user: "cadu@gmail.com",
+//       product: 'Shape Primitive Tiago Lemos 8.0',
+//       price: {
+//         value: 0.00,
+//         currency: "free"
+//       }
+//     };
+
+//     return (
+//       <div className="App">
+//         <h1> Orders recently created </h1>
+//         <Order order={headphone} />
+//         <Order order={energyDrink} />
+//         <Order order={shape} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
