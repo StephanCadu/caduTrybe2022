dbc50b52fe7b077d05be93558e7e03a958a8d3a7
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-07-28 11:54:52 -0300

    exercício de fixação 1

--- FrontEnd/12.CicloVidaComponentes-ReactRouter/12-1.Ciclo-de-vida-de-componentes/life-cicle-practice/src/App.js
+++ FrontEnd/12.CicloVidaComponentes-ReactRouter/12-1.Ciclo-de-vida-de-componentes/life-cicle-practice/src/App.js
@@ -2,13 +2,42 @@ import './App.css';
 import { Component } from 'react';
 // import Counter from './Counter';
 // import RickMorty from './RickMorty';
-import DadJoke from './DadJoke';
+// import DadJoke from './practice/DadJoke';
+import Profile from './components/Profile';
+import Connections from './components/Connections';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showProfile: true,
+    };
+
+    this.changeProfile = this.changeProfile.bind(this);
+  }
+
+  changeProfile() {
+    const { showProfile } = this.state;
+
+    this.setState({ showProfile: !showProfile });
+  }
+
   render() {
+    const { showProfile } = this.state;
     return (
-      <div>
-        <DadJoke />
+      <div className="gitNetwork d-flex flex-column justify-content-center">
+        { showProfile ? <Profile /> : null }
+        <div className="central d-flex justify-content-center">
+          <button
+            className="btn btn-dark align-self-center"
+            type="button"
+            onClick={ this.changeProfile }
+          >
+            Mostrar / Ocultar Perfil
+          </button>
+        </div>
+        <Connections />
       </div>
     );
   }
