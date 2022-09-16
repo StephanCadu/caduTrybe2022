89129b23102410db75293beeb04d13988f3f0004
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-09-16 18:42:51 -0300

    exercício de fixação

---
+++ FrontEnd/17.ContextAPI-ReactHooks/17-3.React-Hooks-useEffect-e-Hooks-customizados/hooks-practice/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RandomTest from './components/RandomTest';
+
+function App() {
+  return (
+    <div className="App">
+      <RandomTest />
+    </div>
+  );
+}
+
+export default App;
---
+++ FrontEnd/17.ContextAPI-ReactHooks/17-3.React-Hooks-useEffect-e-Hooks-customizados/hooks-practice/src/context/Provider.js
@@ -0,0 +1,20 @@
+// import React from 'react'
+// import { useState } from 'react'
+// import RandomContext from './RandomContext'
+
+// export default function Provider({ children }) {
+//   const [timer, setTimer] = useState(0);
+//   const [randomNumber, setRandomNumber] = useState(Math.random() * 100);
+
+
+
+//   const context = {
+
+//   }
+
+//   return (
+//     <RandomContext.Provider value={ context }>
+//       { children }
+//     </RandomContext.Provider>
+//   )
+// }
