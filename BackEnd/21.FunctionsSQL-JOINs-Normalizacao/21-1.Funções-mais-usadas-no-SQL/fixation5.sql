65ccb01634983c8fc833140b3d2b8a269b23e53d
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-10-17 17:50:24 -0300

    📝 notes about COUNT and COALESCE

--- BackEnd/21.FunctionsSQL-JOINs-Normalizacao/21-1.Funções-mais-usadas-no-SQL/fixation5.sql
+++ BackEnd/21.FunctionsSQL-JOINs-Normalizacao/21-1.Funções-mais-usadas-no-SQL/fixation5.sql
@@ -14,3 +14,12 @@ SELECT
     SUM(length) AS 'Tempo de Exibição Total',
     COUNT(length) AS 'Filmes Registrados'
 FROM film;
+
+-- a função COUNT não conta valores nulos,
+-- porém existe a função coalesce que
+-- transforma um valor nulo em qualquer coisa.
+-- a sintaxe é: coalesce(coluna, 'valor')
+
+-- então quando usar o COUNT com uma tabela que
+-- pode ter valores nulos, usa-se:
+-- COUNT(COALESCE(coluna, 'valor'))
