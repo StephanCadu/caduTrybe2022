424ed32ee92acd815afa7e25dc39b753536d176e
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-10-17 10:54:11 -0300

    📌 fixation exercises: strings manipulation

---
+++ BackEnd/21.FunctionsSQL-JOINs-Normalizacao/21-1.Funções-mais-usadas-no-SQL/practice.sql
@@ -0,0 +1,40 @@
+-- funções de manipulação de stings
+
+-- Converte o texto da string para CAIXA ALTA
+SELECT UCASE('Oi, eu sou uma string');
+
+-- Converte o texto da string para caixa baixa
+SELECT LCASE('Oi, eu sou uma string');
+
+-- Substitui as ocorrências de uma substring em uma string
+SELECT REPLACE('Oi, eu sou uma string', 'string', 'cadeia de caracteres');
+
+-- Retorna a parte da esquerda de uma string de acordo com o
+-- número de caracteres especificado
+SELECT LEFT('Oi, eu sou uma string', 3);
+
+-- Retorna a parte da direita de uma string de acordo com o
+-- número de caracteres especificado
+SELECT RIGHT('Oi, eu sou uma string', 6);
+
+-- Exibe o tamanho, em caracteres, da string, a função LENGTH retorna o tamanho em bytes
+SELECT CHAR_LENGTH('Oi, eu sou uma string');
+
+-- Extrai parte de uma string de acordo com o índice de um caractere inicial
+-- e a quantidade de caracteres a extrair
+SELECT SUBSTRING('Oi, eu sou uma string', 5, 2);
+
+-- Se a quantidade de caracteres a extrair não for definida,
+-- então a string será extraída do índice inicial definido, até o seu final
+SELECT SUBSTRING('Oi, eu sou uma string', 5);
+
+-- prática
+
+SELECT UCASE(title) FROM film LIMIT 10;
+SELECT LCASE(title) FROM film LIMIT 10;
+SELECT REPLACE(title, 'ACADEMY', 'DINOSAUR') FROM film WHERE film_id = 1;
+SELECT LEFT(title, 7) FROM film WHERE film_id = 1;
+SELECT RIGHT(title, 8) FROM film WHERE film_id = 1;
+SELECT CHAR_LENGTH(title) FROM film WHERE film_id = 1;
+SELECT SUBSTRING(title, 5, 2) FROM film WHERE film_id = 1;
+SELECT SUBSTRING(title, 5) FROM film WHERE film_id = 1;
