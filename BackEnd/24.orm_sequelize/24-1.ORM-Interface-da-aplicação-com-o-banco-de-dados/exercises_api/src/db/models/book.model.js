31d873a57a1ef2be71ce26b2f6643cb45a2db409
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-11-10 21:15:37 -0300

    📚 ex 2 creating book model

---
+++ BackEnd/24.orm_sequelize/24-1.ORM-Interface-da-aplicação-com-o-banco-de-dados/exercises_api/src/db/models/book.model.js
@@ -0,0 +1,12 @@
+const BookModel = (Sequelize, DataTypes) => {
+  const Book = Sequelize.define('Book', {
+    title: DataTypes.STRING,
+    author: DataTypes.STRING,
+    pageQuantity: DataTypes.INTEGER,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  });
+  return Book;
+};
+
+module.exports = BookModel;
