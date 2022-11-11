2ede62cda3a93d9f23a41f3464926d2bc5e983d4
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-11-10 21:26:55 -0300

    📚 exs 3, 4 and 5 - creating book service, book model and route get

--- BackEnd/24.orm_sequelize/24-1.ORM-Interface-da-aplicação-com-o-banco-de-dados/exercises_api/index.js
+++ BackEnd/24.orm_sequelize/24-1.ORM-Interface-da-aplicação-com-o-banco-de-dados/exercises_api/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const bookRouter = require('./src/routers/book.router');
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+
+app.use('/books', bookRouter);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
