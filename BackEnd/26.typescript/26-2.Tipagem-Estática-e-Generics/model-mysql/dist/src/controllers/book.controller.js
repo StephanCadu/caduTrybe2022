b4685486ba0c5464391c379d4adf47b32703dcaf
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-11-23 12:03:37 -0300

    📌 fixation exercises: executing tsc to create js files

---
+++ BackEnd/26.typescript/26-2.Tipagem-Estática-e-Generics/model-mysql/dist/src/controllers/book.controller.js
@@ -0,0 +1,9 @@
+"use strict";
+// import bookService from '../services/book.service';
+// const getBooks = async (_req: Request, res: Response) => {
+//   const books = await bookService.getBooks();
+//   return res.status(200).json(books);
+// };
+// export default {
+//   getBooks,
+// };
