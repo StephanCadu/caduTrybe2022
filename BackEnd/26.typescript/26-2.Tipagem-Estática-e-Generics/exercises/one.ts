a0594b71fb8909a97a356763f6b056de2c52c695
Carlos Eduardo Stephan <cdauk@hotmail.com>
2022-11-23 15:01:40 -0300

    📚 exercise 1: creating class Car

---
+++ BackEnd/26.typescript/26-2.Tipagem-Estática-e-Generics/exercises/one.ts
@@ -0,0 +1,48 @@
+interface Car {
+  brand: string,
+  color: string,
+  doors: number,
+  honk(): void,
+  turnOn(): void,
+  turnOff(): void,
+  speedUp(): void,
+  speedDown(): void,
+  stop(): void,
+  turn(direction: string): void,
+}
+
+class Car {
+  constructor(brand: string, color: string, doors: number) {
+    this.brand = brand;
+    this.color = color;
+    this.doors = doors;
+  }
+
+  honk(): void {
+    console.log('BI BIIIIIII');
+  }
+
+  turnOn(): void {
+    console.log('Car turned on');
+  }
+
+  turnOff(): void {
+    console.log('Car turned off');
+  }
+
+  speedUp(): void {
+    console.log('Car speed up');
+  }
+
+  speedDown(): void {
+    console.log('Car speed down');
+  }
+
+  stop(): void {
+    console.log('Car stops');
+  }
+
+  turn(direction: string) {
+    console.log(`Car turns ${direction}`);
+  }
+}
