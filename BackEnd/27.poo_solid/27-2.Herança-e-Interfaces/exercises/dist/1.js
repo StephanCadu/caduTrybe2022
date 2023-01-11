4afdf4359b9ffa74ec09ebf87ee93b77dcf693b1
Carlos Eduardo Stephan <cadu@MacBook-Air-de-Carlos.local>
2023-01-11 15:41:12 -0300

    📚 exercise 1: creating Person class

---
+++ BackEnd/27.poo_solid/27-2.Herança-e-Interfaces/exercises/dist/1.js
@@ -0,0 +1,27 @@
+"use strict";
+class Person {
+    constructor(name, birth) {
+        if (name.length < 3)
+            this._name = '';
+        else {
+            this._name = name;
+        }
+        const curDate = new Date();
+        const thisYear = curDate.getFullYear();
+        const birthYear = +birth.split('/')[2];
+        console.log(thisYear, birthYear, thisYear - birthYear > 120);
+        if (birthYear > thisYear
+            || thisYear - birthYear > 120)
+            this._birthDate = `1/1/${thisYear}`;
+        else {
+            this._birthDate = birth;
+        }
+    }
+    get name() { return this._name; }
+    get birthDate() { return this._birthDate; }
+    set name(newName) { this._name = newName; }
+}
+const rightPerson = new Person('Camila', '12/3/2040');
+const wrongPerson = new Person('Ca', '12/3/1850');
+console.log(rightPerson.birthDate);
+console.log(wrongPerson.birthDate);
---
+++ BackEnd/27.poo_solid/27-2.Herança-e-Interfaces/exercises/src/1.ts
@@ -0,0 +1,39 @@
+class Person {
+  private _name: string
+  private _birthDate: string
+
+  constructor(
+    name: string,
+    birth: string
+  ) {
+    if (name.length < 3) this._name = '';
+    else {
+      this._name = name;
+    }
+
+    const curDate = new Date();
+    const thisYear = curDate.getFullYear();
+    const birthYear = +birth.split('/')[2];
+    console.log(thisYear, birthYear, thisYear -  birthYear > 120);
+    
+    if (
+      birthYear > thisYear
+      || thisYear -  birthYear > 120
+    ) this._birthDate = `1/1/${thisYear}`;
+    else {
+      this._birthDate = birth;
+    }
+  }
+
+  get name() { return this._name }
+
+  get birthDate() { return this._birthDate }
+
+  set name(newName: string) { this._name = newName }
+}
+
+const rightPerson = new Person('Camila', '12/3/2040');
+const wrongPerson = new Person('Ca', '12/3/1850');
+
+console.log(rightPerson.birthDate);
+console.log(wrongPerson.birthDate);
