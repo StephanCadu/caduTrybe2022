c098153bc8f32c4e91bfc78ff043b1818e026e5c
Carlos Eduardo Stephan <cadu@MacBook-Air-de-Carlos.local>
2023-01-12 09:49:30 -0300

    finishing exercises

--- BackEnd/27.poo_solid/27-3.Polimorfismo/exercises/src/QuadraFutebol.ts
+++ BackEnd/27.poo_solid/27-3.Polimorfismo/exercises/src/QuadraFutebol.ts
@@ -2,13 +2,14 @@ import IAgenda from "./interfaces/IAgenda";
 import IFutebol from "./interfaces/IFutebol";
 import normas from "./normas/normasDeUso";
 import Quadra from "./Quadra";
+import randomProtocol from "./utils/protocol";
 
 export default class QuadraFutebol extends Quadra {
   public data: IFutebol = normas.futebol
 
   reservar<IFutebol>(date: Date = new Date()): IAgenda<IFutebol> {
     return {
-      protocolo: 'q39tgorea9',
+      protocolo: randomProtocol(),
       data: date,
       regras: this.data as IFutebol,
     }
--- BackEnd/27.poo_solid/27-3.Polimorfismo/fixation/src/polymorphism.ts
+++ BackEnd/27.poo_solid/27-3.Polimorfismo/fixation/src/polymorphism.ts
@@ -33,3 +33,82 @@ const invoker = new LongRangeCharacter();
 
 characterInteract(axe);
 characterInteract(invoker);
+
+// using interfaces and generics
+interface ICharacter {
+  name: string;
+  specialMove: string;
+}
+
+interface DbCharacter extends ICharacter {
+  id: number;
+}
+
+const db: DbCharacter[] = [];
+
+interface IModel {
+  create(char: ICharacter): DbCharacter
+  findAll(): DbCharacter[]
+  findById(id: number): DbCharacter
+  update(id: number, char: ICharacter): DbCharacter
+  delete(id: number): boolean
+}
+
+class LocalDbModel implements IModel {
+  create(char: ICharacter): DbCharacter {
+    const newChar = { ...char, id: db.length }
+    db.push(newChar);
+    return newChar
+  }
+
+  findAll(): DbCharacter[] {
+    return db;
+  }
+
+  findById(id: number): DbCharacter {
+    const found = db.find((cha) => cha.id === id);
+    return found !== undefined ? found : { name: '', id: 0, specialMove: '' }
+  }
+
+  update(id: number, char: DbCharacter): DbCharacter {
+    const found = db.find((elm) => elm.id === id);
+    if (found) {
+      const index = db.indexOf(found);
+      db[index] = char;
+      return char;
+    }
+    return char;
+  }
+
+  delete(id: number): boolean {
+    const found = db.find((elm) => elm.id === id);
+    if (found) {
+      const index = db.indexOf(found);
+      db.splice(index, 1)
+      return true;
+    }
+    return false
+  }
+}
+
+class CharacterService {
+  constructor(public db: IModel = new LocalDbModel()) {}
+}
+
+const naruto = { name: 'Naruto', id: 33, specialMove: 'Rasengan' }
+const sasuke = { name: 'Sasuke', id: 32, specialMove: 'Chidori' }
+
+class MockedDbModel implements IModel {
+  create(char: ICharacter): DbCharacter { return sasuke }
+
+  findAll(): DbCharacter[] { return [sasuke, naruto] }
+
+  findById(id: number): DbCharacter { return naruto }
+
+  update(id: number, char: DbCharacter): DbCharacter { return naruto }
+
+  delete(id: number): boolean { return true }
+}
+
+const test1 = new CharacterService(new LocalDbModel());
+const test2 = new CharacterService(new MockedDbModel());
