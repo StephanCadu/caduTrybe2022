a8012abb3304d12577d5da8897b2bc4d4c32d7ec
Carlos Eduardo Stephan <cadu@MacBook-Air-de-Carlos.local>
2023-03-27 18:33:05 -0300

    altera arquivo readcsv.py

--- computer_science/01.intro-python/01-2.data-entrance-exit/csv/readcsv.py
+++ computer_science/01.intro-python/01-2.data-entrance-exit/csv/readcsv.py
@@ -1,11 +1,12 @@
 import csv
 
-# with open("graduacao_unb.csv", encoding="utf-8") as file:
-#     graduacao_reader = csv.reader(file, delimiter=",", quotechar='"')
-#     # Usando o conceito de desempacotamento
-#     header, *data = graduacao_reader
+with open("graduacao_unb.csv", encoding="utf-8") as file:
+    graduacao_reader = csv.reader(file, delimiter=",", quotechar='"')
+    # Usando o conceito de desempacotamento
+    header, *data = graduacao_reader
 
-# print(data)
+print(data)
+print(header)
 
 
 with open("graduacao_unb.csv", encoding="utf8") as file:
