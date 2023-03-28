ae547f474a40ab00d9798e751fe351603c51b189
Carlos Eduardo Stephan <cadu@MacBook-Air-de-Carlos.local>
2023-03-28 16:53:28 -0300

    📚 exercises 2 and 3: creating statistics and geometric figures classes

---
+++ computer_science/02.design-patterns/02-1.poo-python/ex3.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+from math import pi
+
+
+class FiguraGeometrica(ABC):
+    @abstractmethod
+    def area(self):
+        ...
+
+    @abstractmethod
+    def perimetro(self):
+        ...
+
+
+class Quadrado(FiguraGeometrica):
+    pass
+
+    def __init__(self, lado):
+        self.lado = lado
+
+    def area(self):
+        return self.lado ** 2
+
+    def perimetro(self):
+        return 4 * self.lado
+
+
+class Retangulo(FiguraGeometrica):
+    pass
+
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+
+    def area(self):
+        return self.base * self.altura
+
+    def perimetro(self):
+        return (self.base + self.altura) * 2
+
+
+class Circulo(FiguraGeometrica):
+    pass
+
+    def __init__(self, raio):
+        self.raio = raio
+
+    def area(self):
+        return pi * (self.raio ** 2)
+
+    def perimetro(self):
+        return 2 * pi * self.raio
