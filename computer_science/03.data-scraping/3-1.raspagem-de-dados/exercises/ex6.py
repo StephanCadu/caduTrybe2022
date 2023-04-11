4548b99b4bb81521f1be263090df4cc2fb49326b
Carlos Eduardo Stephan <cadu@MacBook-Air-de-Carlos.local>
2023-04-11 20:08:55 -0300

    📚 exercise 7: filtering by category and sorting published books

--- computer_science/03.data-scraping/3-1.raspagem-de-dados/exercises/ex6.py
+++ computer_science/03.data-scraping/3-1.raspagem-de-dados/exercises/ex6.py
@@ -1,15 +1,9 @@
 from pymongo import MongoClient
 
 
-client = MongoClient()
-
-db = client.library
-
-category = input("Digite a categoria escolhida: ")
-
-print(db.books.find_one())
-
 with MongoClient() as client:
     db = client.library
+    category = input("Digite a categoria escolhida: ")
+
     for book in db.books.find({"categories": category}):
         print(book["title"])
