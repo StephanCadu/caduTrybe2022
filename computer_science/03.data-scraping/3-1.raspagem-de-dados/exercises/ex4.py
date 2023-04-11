77cb0f49097d29a81c483fbdf3ce9e3d85f560eb
Carlos Eduardo Stephan <cadu@MacBook-Air-de-Carlos.local>
2023-04-11 15:08:59 -0300

    📚 exercise 4: scrapping book info

---
+++ computer_science/03.data-scraping/3-1.raspagem-de-dados/exercises/ex4.py
@@ -0,0 +1,31 @@
+import requests
+from parsel import Selector
+
+
+url = "http://books.toscrape.com"
+
+endpoint = "/catalogue/the-grand-design_405/index.html"
+
+response = requests.get(f"{url}{endpoint}")
+
+selector = Selector(text=response.text)
+
+title = selector.css(".product_main > h1::text").get()
+
+regex = r"£\d+\.\d{2}"
+price = selector.css(".price_color::text").re_first(regex)
+
+description = selector.css(".sub-header + p::text").get()
+
+suffix = "...more"
+
+print(description.endswith(suffix))
+if description.endswith(suffix):
+    description = description[: -len(suffix)]
+
+src_url = selector.css(".item.active > img::attr(src)").get()
+src_url = src_url.replace("../..", "")
+
+img_url = f"{url}{src_url}"
+
+print(title, price, description, img_url)
