9d00d91e70b9334a08e48d188979166a17cfa62d
Cadu <cdauk@hotmail.com>
2023-05-11 15:35:02 -0300

    📌 fixation: creating hash_example file

---
+++ computer_science/06.data-structureII/06-1.Hashmap-e-Dict/hash_example.py
@@ -0,0 +1,43 @@
+class Employee:
+    def __init__(self, id, name) -> None:
+        self.id = id
+        self.name = name
+
+
+class HashMap:
+    def __init__(self) -> None:
+        self.buckets = [None for i in range(10)]
+
+    def get_address(self, id_num):
+        return id_num % 10
+
+    def insert(self, employee):
+        address = self.get_address(employee.id)
+        self.buckets[address] = employee
+
+    def get_value(self, id):
+        address = self.get_address(id)
+        return self.buckets[address].name
+
+    def has(self, id):
+        address = self.get_address(id)
+        return self.buckets[address] is not None
+
+    def update_value(self, id, new_name):
+        address = self.get_address(id)
+        self.buckets[address] = Employee(id, new_name)
+
+
+employees = [(14, "name1"), (23, "name2"), (10, "name3"), (9, "name4")]
+my_hash = HashMap()
+
+for id, name in employees:
+    my_hash.insert(Employee(id, name))
+
+print(my_hash.get_value(23))
+
+print(my_hash.get_value(10))
+
+my_hash.update_value(10, "name30")
+
+print(my_hash.get_value(10))
